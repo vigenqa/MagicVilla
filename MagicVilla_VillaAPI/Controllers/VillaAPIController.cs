599d1d1b722a7ba5116412ac353a9c90b59a5d31
Vigen <vigen.smbatyan@gmail.com>
2024-02-02 12:07:04 +0400

    Add PUT and PATCH update methods

--- MagicVilla_VillaAPI/Controllers/VillaAPIController.cs
+++ MagicVilla_VillaAPI/Controllers/VillaAPIController.cs
@@ -1,5 +1,6 @@
 ﻿using MagicVilla_VillaAPI.Data;
 using MagicVilla_VillaAPI.Models.Dto;
+using Microsoft.AspNetCore.JsonPatch;
 using Microsoft.AspNetCore.Mvc;
 
 namespace MagicVilla_VillaAPI.Controllers
@@ -75,7 +76,45 @@ namespace MagicVilla_VillaAPI.Controllers
             }
             VillaStore.villaList.Remove(villa);
             return NoContent();
+        }
 
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [HttpPut("{id:int}", Name = "UpdateVilla")]
+        public IActionResult UpdateVilla(int id, [FromBody] VillaDto villaDto)
+        {
+            if (villaDto == null || id != villaDto.Id)
+            {
+                return BadRequest();
+            }
+            var villa = VillaStore.villaList.FirstOrDefault(u => u.Id == id);
+            villa.Name = villaDto.Name;
+            villa.Sqft = villaDto.Sqft;
+            villa.Occupancy = villaDto.Occupancy;
+
+            return NoContent();
+        }
+
+        [HttpPatch("{id:int}", Name = "UpdatePartialVilla")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public IActionResult UpdatePartialVilla(int id, JsonPatchDocument<VillaDto> patchDto)
+        {
+            if (patchDto == null || id == 0)
+            {
+                return BadRequest();
+            }
+            var villa = VillaStore.villaList.FirstOrDefault(u => u.Id == id);
+            if(villa == null)
+            {
+                return BadRequest();
+            }
+            patchDto.ApplyTo(villa, ModelState);
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            return NoContent();
         }
     }
 }
--- MagicVilla_VillaAPI/Data/VillaStore.cs
+++ MagicVilla_VillaAPI/Data/VillaStore.cs
@@ -5,8 +5,9 @@ namespace MagicVilla_VillaAPI.Data
     public static class VillaStore
     {
         public static List<VillaDto> villaList = new List<VillaDto> {
-            new VillaDto{ Id = 1,Name= "pool View"},
-            new VillaDto{ Id = 2,Name="Beach Vew"}
+            new VillaDto{ Id = 1,Name= "pool View",Sqft=100,Occupancy=4},
+            new VillaDto{ Id = 2,Name="Beach Vew",Sqft=300,Occupancy=3}
+
             };
     }
 }
